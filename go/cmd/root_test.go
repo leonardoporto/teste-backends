d80b6e331ca2e38df5eb311dce2ff3b819296477
Leonardo Porto <leonardo.porto.souza@gmail.com>
2020-02-17 13:00:26 -0300

    docs(readme): add readme and makefile

--- go/cmd/root_test.go
+++ go/cmd/root_test.go
@@ -11,16 +11,16 @@ func TestCheckProposals(t *testing.T) {
 		in   string
 		out  int
 	}{
-		// {"000", "../../test/input/input000.txt", 3},
-		// {"001", "../../test/input/input001.txt", 3},
-		// {"002", "../../test/input/input002.txt", 3},
-		// {"003", "../../test/input/input003.txt", 3},
-		// {"004", "../../test/input/input004.txt", 2},
-		// {"005", "../../test/input/input005.txt", 3},
-		// {"006", "../../test/input/input006.txt", 3},
-		// {"007", "../../test/input/input007.txt", 3},
-		// {"008", "../../test/input/input008.txt", 3},
-		{"009", "../../test/input/input009.txt", 1},
+		{"000", "../../test/input/input000.txt", 3},
+		{"001", "../../test/input/input001.txt", 3},
+		{"002", "../../test/input/input002.txt", 3},
+		{"003", "../../test/input/input003.txt", 3},
+		{"004", "../../test/input/input004.txt", 2},
+		{"005", "../../test/input/input005.txt", 3},
+		{"006", "../../test/input/input006.txt", 3},
+		{"007", "../../test/input/input007.txt", 3},
+		{"008", "../../test/input/input008.txt", 2},
+		{"009", "../../test/input/input009.txt", 4},
 	}
 
 	for _, tt := range tests {
